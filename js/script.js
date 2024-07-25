d00f45ef474f8b40f2f93aa5d63e847cec47ee92
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-07-25 14:30:31 +0200

    [UPD] exercise done

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            emails: []
+        }
+    },
+    methods: {
+        generateTenRandomEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+                    this.emails.push(resp.data.response)
+                })
+            }
         }
     }
 }).mount('#app')
